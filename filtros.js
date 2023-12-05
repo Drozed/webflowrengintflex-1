6dd3a94754633b06a0de7e8cde280bded628fbb1
David Osorio <david.osorio5630@gmail.com>
2023-12-05 18:56:50 -0500

    Release

--- filtros.js
+++ filtros.js
@@ -3,17 +3,17 @@
   console.log("alerta");
   //Obtengo las marcas que estan en los divs de la collection
   let atributostoshow = [];
-  let  marcas = document.querySelectorAll('[dv-filtros-campo="Marca"]');
-  marcas.forEach(function(elemento){
-    elemento.setAttribute("cms-marca",elemento.innerHTML);
+  let marcas = document.querySelectorAll('[dv-filtros-campo="Marca"]');
+  marcas.forEach(function (elemento) {
+    elemento.setAttribute("cms-marca", elemento.innerHTML);
     let atributo = elemento.getAttribute("cms-marca")
     atributostoshow.push(atributo);
   })
 
-  
+
   let checkboxes = document.querySelectorAll('[chk="Marca"]');
-  checkboxes.forEach(function(elemento){
-    elemento.setAttribute("Marca",elemento.querySelector("span").innerHTML);
+  checkboxes.forEach(function (elemento) {
+    elemento.setAttribute("Marca", elemento.querySelector("span").innerHTML);
   })
 
   showCheckboxes(atributostoshow);
@@ -21,7 +21,7 @@
   function showCheckboxes(checkboxesToDisplay) {
 
     // Muestra solo las checkboxes que están en el arreglo
-    checkboxesToDisplay.forEach(function(atributo) {
+    checkboxesToDisplay.forEach(function (atributo) {
       console.log(atributo);
       let checkbox = document.querySelector(`[marca="${atributo}"]`);
       console.log(checkbox);
@@ -39,44 +39,47 @@
 
   //FILTRO LINEAS
 
-  let txtFiltroLinea = document.querySelector('[txtFiltro="Linea"]')
-  let chkLinea = document.querySelectorAll('[chk="Linea"]');
-  chkLinea.forEach(function(elemento){
-    elemento.setAttribute("Linea",elemento.querySelector("span").innerHTML);
-  })
+  let txtFiltroMarca = document.querySelector('[txtFiltro="Marca"]')
+  let chkMarca = document.querySelectorAll('[chk="Marca"]');
+  // chkLinea.forEach(function (elemento) {
+  //   elemento.setAttribute("Marca", elemento.querySelector("span").innerHTML);
+  // })
 
   //Filtro Top 10
-  showCheckboxesRange(10);
-  
+  // showCheckboxesRange(10);
 
-  function showCheckboxesRange(numToShow) {
-    // Obtiene todas las checkboxes
-    let chkLinea = document.querySelectorAll('[chk="Linea"]');
 
-    // Oculta todas las checkboxes
-    // chkLinea.forEach(function (checkbox) {
-    //   checkbox.classList.add("hidden");
-    // });
+  // function showCheckboxesRange(numToShow) {
+  //   // Obtiene todas las checkboxes
+  //   let chkLinea = document.querySelectorAll('[chk="Linea"]');
 
-    // Muestra las primeras 'numToShow' checkboxes
-    for (let i = 0; i < numToShow; i++) {
-      chkLinea[i].classList.remove("c-hidden");
-    }
-  }
+  //   // Oculta todas las checkboxes
+  //   // chkLinea.forEach(function (checkbox) {
+  //   //   checkbox.classList.add("hidden");
+  //   // });
 
-  txtFiltroLinea.addEventListener('input',function(){
-    let filtroTexto = txtFiltroLinea.value.toLowerCase();
+  //   // Muestra las primeras 'numToShow' checkboxes
+  //   for (let i = 0; i < numToShow; i++) {
+  //     chkLinea[i].classList.remove("c-hidden");
+  //   }
+  // }
 
-    chkLinea.forEach(function(chk){
-      let atributo = chk.getAttribute("Linea".toLowerCase());
+  txtFiltroMarca.addEventListener('input', function () {
+    let filtroTexto = txtFiltroMarca.value.toLowerCase();
+
+    chkMarca.forEach(function (chk) {
+      let atributo = chk.getAttribute("Marca".toLowerCase());
       let textoVisible = atributo.includes(filtroTexto);
 
-      if(textoVisible){
-        chk.classList.remove("c-hidden");
+      if (filtroTexto.length > 0) {
+        if (textoVisible) {
+          chk.classList.remove("c-hidden");
+        } else {
+          chk.classList.add("c-hidden");
+        }
       }else{
-        chk.classList.add("c-hidden");
+        showCheckboxes(atributostoshow);
       }
-
     });
   });
 
